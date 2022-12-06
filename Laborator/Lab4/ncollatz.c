2a74d4b4844c17dcdff92b4d43d4eaa6ef03f5de
davidpatranjel <davidpatranjel24@gmail.com>
2022-12-06 02:05:27 +0200

    -modif ncollatz

--- Laborator/Lab4/ncollatz.c
+++ Laborator/Lab4/ncollatz.c
@@ -7,23 +7,31 @@
 #include <sys/wait.h>
 int main(int argc, char ** args){
 
-	if( argc >= 2){
+    if( argc >= 2){
+		printf("Starting parent %d\n", getpid());
 		for(int i = 1; i < argc; ++i){
-			char* number = args[i];
+			int number = atoi(args[i]);
 			pid_t pid = fork();
 			if(pid < 0)
 				return errno ;
 			else if(pid == 0){
 				/* child instructions */
-				char* argv [] = {"./c", number ,NULL };
-				execve(argv[0] ,argv ,NULL );
+				printf("%d : ", number);
+				while(number != 1){
+					printf("%d ", number);
+					if(number % 2 == 0) number /= 2;
+					else number = 3 * number + 1;
+				}
+				printf("%d \n", 1);
+				printf("Done Parent %d Me %d\n", getppid(), getpid());
 				perror(NULL);
-			}else{
-				/* parent instructions */
-				wait(NULL);
-				printf("Child %d finished\n", pid); ///in pid avem cel al copilului
+				exit(0);
 			}
 		}
+		for(int i = 1; i < argc; ++i){
+			wait(NULL);  
+		}
+		printf("Done Parent %d Me %d\n", getppid(), getpid());
 	}else{
 		char* error = "Eroare! Args insuficiente!";
 		write(1, error, strlen(error));
